2327c40403103a4822b572b0bbb9f923bd5ff64c
MohammadNikmard <mnikmard2004@gmail.com>
2025-04-14 11:40:08 +0330

    chore: update site

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2769203477"
+    serviceWorkerVersion: "3926393469"
   }
 });
