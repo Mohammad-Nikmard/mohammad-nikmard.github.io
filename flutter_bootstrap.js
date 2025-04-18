01971aaee8bbc3322ea6d8b6e5722f1a552bc70f
MohammadNikmard <mnikmard2004@gmail.com>
2025-04-18 09:29:10 +0330

    chore: lil bit of update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3926393469"
+    serviceWorkerVersion: "1505528741"
   }
 });
